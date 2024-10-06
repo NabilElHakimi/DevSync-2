e62a41f1fa492f53fb540aad9f26c6e1cc19d100
EL-Nabil-Hakimi <hakimi.deve@gmail.com>
2024-10-06 15:46:09 +0100

    fixing service

--- src/main/java/org/example/DevSync2/repository/TokenRepository.java
+++ src/main/java/org/example/DevSync2/repository/TokenRepository.java
@@ -1,4 +1,62 @@
 package org.example.DevSync2.repository;
 
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityManagerFactory;
+import jakarta.persistence.Persistence;
+import org.example.DevSync2.entity.Token;
+
+import java.util.List;
+
 public class TokenRepository {
+
+    private final EntityManagerFactory emf = Persistence.createEntityManagerFactory("default");
+
+    public boolean save(Token token) {
+        EntityManager em = emf.createEntityManager();
+        em.getTransaction().begin();
+        em.persist(token);
+        em.getTransaction().commit();
+        em.close();
+        return true;
+    }
+
+    public boolean update(Token token) {
+        EntityManager em = emf.createEntityManager();
+        em.getTransaction().begin();
+        em.merge(token);
+        em.getTransaction().commit();
+        em.close();
+        return true;
+    }
+
+    public boolean delete(Token token) {
+        EntityManager em = emf.createEntityManager();
+        em.getTransaction().begin();
+        em.remove(token);
+        em.getTransaction().commit();
+        em.close();
+        return true;
+    }
+
+
+    public Token findById(Long id) {
+        EntityManager em = emf.createEntityManager();
+        em.getTransaction().begin();
+        Token token = em.find(Token.class, id);
+        em.getTransaction().commit();
+        em.close();
+        return token;
+    }
+
+
+    public List<Token> findAll() {
+        EntityManager em = emf.createEntityManager();
+        em.getTransaction().begin();
+        List<Token> tokens = em.createQuery("SELECT c FROM Token c ORDER BY c.id DESC", Token.class).getResultList();
+        em.getTransaction().commit();
+        em.close();
+        return tokens;
+    }
+
+
 }
--- src/main/java/org/example/DevSync2/service/TokenService.java
+++ src/main/java/org/example/DevSync2/service/TokenService.java
@@ -1,6 +1,16 @@
 package org.example.DevSync2.service;
 
+import org.example.DevSync2.entity.Token;
+import org.example.DevSync2.repository.TokenRepository;
+
 public class TokenService {
+
+    private final TokenRepository tokenRepository = new TokenRepository();
+
+    public boolean save(Token token) {
+        return tokenRepository.save(token);
+    }
+
 
 
 }
