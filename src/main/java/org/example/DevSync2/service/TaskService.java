5528cd4d9d62d4036ec13b9b88261fafb7268f62
EL-Nabil-Hakimi <hakimi.deve@gmail.com>
2024-10-06 15:41:53 +0100

    add Tasks services

--- src/main/java/org/example/DevSync2/service/TaskService.java
+++ src/main/java/org/example/DevSync2/service/TaskService.java
@@ -1,4 +1,34 @@
 package org.example.DevSync2.service;
 
+import org.example.DevSync2.entity.Task;
+import org.example.DevSync2.repository.TaskRepository;
+
+import java.util.List;
+
 public class TaskService {
+
+    private final TaskRepository taskRepository = new TaskRepository();
+
+    public boolean save(Task task) {
+        return taskRepository.save(task);
+    }
+
+    public boolean delete(Task task) {
+        return taskRepository.delete(task);
+    }
+
+    public boolean update(Task task) {
+        return taskRepository.update(task);
+    }
+
+    public Task findById(Long id) {
+        return taskRepository.findById(id);
+    }
+
+    public List<Task> findAll() {
+        return taskRepository.findAll();
+    }
+
+
+
 }
